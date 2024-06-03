35859f023be01914fbf658ee0851f0efe46ac2be
OrawusiTimilehin <oluwatimilehin.orawusi@gmail.com>
2024-06-03 16:48:58 +0100

    Created post creation form

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from 'pg';
+import env from 'dotenv';
 
 const app = express();
 const port = 3000;
 
+
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded(  { extended:true }  ))
 
 app.get("/", (req, res) => {
     res.render("index.ejs");
@@ -28,6 +32,20 @@ app.get("/post/", (req, res )=>{
 
 app.get("/create",  (req, res)=>{
     res.render("create.ejs");
+})
+
+app.post("/add-post", async (req, res)=>{
+    const title = req.body.title;
+    const author = req.body.author;
+    const publish_date = req.body.publish_date;
+    const content = req.body.content;
+    const category = req.body.category;
+    const tags = req.body.tags;
+    const description= req.body.meta_description;
+    const time = req.body.reading_time;
+
+    cosnt result = await debugger.qu
+
 })
 
 
