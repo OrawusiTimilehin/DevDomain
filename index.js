e0178c2d38362a893c981f1ee94fe8618dbf727d
OrawusiTimilehin <oluwatimilehin.orawusi@gmail.com>
2024-06-11 17:02:08 +0100

    Implemented Passport local Strategy

--- index.js
+++ index.js
@@ -3,18 +3,45 @@ import bodyParser from "body-parser";
 import pg from 'pg';
 import env from 'dotenv';
 import bcrypt from 'bcrypt';
+import session from "express-session";
+import passport from "passport";
+import {Strategy} from "passport-local";
+import flash from 'connect-flash';
 
 //Constant variables
 const app = express();
 const port = 3000;
 const saltRounds = 10;
 
+
 //Configuring environment variable
 env.config();
 
+
 // Middlewares
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded(  { extended:true }  ))
+app.use(bodyParser.urlencoded(  { extended:true }  ));
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized : true,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 8
+    }
+}));
+
+// Error message flash middle ware
+app.use(flash());
+//Passport middle ware has to come after the session middle ware. 
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Setting up the flash in the locals variable
+app.use((req, res, next) => {
+    res.locals.messages = req.flash();
+    next();
+  });
+
 
 // Initialization of the db object
 const db = new pg.Client({
@@ -29,54 +56,52 @@ db.connect();
 // Route Handlers
 
 app.get("/", (req, res) => {
-    res.render("index.ejs");
+     res.render("index.ejs", {
+            authenticated: req.isAuthenticated()
+        });
+
+    
 })
 
 app.get("/posts", async (req, res) => {
     const result  = await db.query("SELECT * FROM posts");
     console.log(result.rows)
     res.render("posts.ejs", {
-        posts : result.rows
-    });
+        posts : result.rows,
+        authenticated: req.isAuthenticated()
+    })
+  
 })
 
 app.get("/login", (req, res) => {
-    res.render("login.ejs");
+    res.render("login.ejs",{
+    authenticated: req.isAuthenticated()
+});
 })
 
-app.post("/login", async (req, res)=>{
-    const email = req.body.email;
-    const password = req.body.password;
+app.post(
+    "/login",
+    passport.authenticate("local", {
+      successRedirect: "/",
+      failureRedirect: "/login",
+      failureFlash: true,
+    })
+  );
 
-    try {
-        const result = await db.query("SELECT * FROM users WHERE email = $1", [email]);
-        if (result.rows.length > 0){
-            const user = result.rows[0]
-            const storedHashedPassword = user.password;
-
-            bcrypt.compare(password,storedHashedPassword, (err, result)=>{
-                if (err){
-                    console.log(err);
-                }else{
-                    //result is a boolean which is either true or false depending on whether the passwords match or not.
-                    if (result){
-                        res.send("Loged In")
-                    }else{
-                        res.send("Wrong password")
-                    }
-                }
-            })
+app.get("/logout", (req, res)=>{
+    req.logout((err)=>{
+        if(err){
+            return next(err);
         }
-;    }catch(err){
-        console.log(err)
-        res.render("login.ejs", {
-            error: "User not found"
-        })
-    }
+        res.redirect("/");
+        
+    });
 })
 
 app.get("/signup", (req, res) => {
-    res.render("signup.ejs", );
+    res.render("signup.ejs", {
+        authenticated: req.isAuthenticated()
+    });
 })
 
 app.post("/signup", async (req, res)=>{
@@ -93,37 +118,56 @@ app.post("/signup", async (req, res)=>{
                 if (err){
                     console.log(err)
                 }else{
-                    const user = await db.query("INSERT INTO users (username, email, password) VALUES ($1, $2, $3)", [username, email, hash]);
+                    const result = await db.query("INSERT INTO users (username, email, password) VALUES ($1, $2, $3) RETURNING *", [username, email, hash]);
+                    const user = result.rows[0];
+                    req.login(user, (err)=>{
+                        if(err){
+                        console.log(err);
+                        }else{
+                            console.log("Success");
+                            res.redirect("/");
+                        }
+                    })
                 }
                 
             });
            
         }else{
             res.render("signup.ejs", {
-                existing_user : true
+                existing_user : true,
+                authenticated: req.isAuthenticated()
             })
         }
     }else{
         console.log("entered else block")
         res.render("signup.ejs", {
-            error: "Passwords do not match"
+            error: "Passwords do not match",
+            authenticated: req.isAuthenticated()
         });
     }
 })
 
 app.get("/post/:id", async (req, res )=>{
-    console.log("Entered into the route handler")
+    if (req.isAuthenticated()){
+        console.log("Entered into the route handler")
     const id = req.params.id;
     console.log(`This is the id ${id}`);
     const result = await db.query("SELECT * FROM posts WHERE id = $1", [id]);
     console.log(result.rows);
     res.render("article.ejs", {
-        post: result.rows[0]
+        post: result.rows[0],
+        authenticated: req.isAuthenticated()
     });
+    }else{
+        res.redirect("/login")
+    }
+    
 })
 
 app.get("/create",  (req, res)=>{
-    res.render("create.ejs");
+    res.render("create.ejs",{
+        authenticated: req.isAuthenticated()
+    });
 })
 
 app.post("/add-post", async (req, res)=>{
@@ -148,7 +192,54 @@ app.post("/add-post", async (req, res)=>{
 
 })
 
+//passport local strategy 
+
+//NOTE : for error messages to show in the 'info' part of the cb it must be an object with 'message: error'. It must be called message.
+passport.use( new Strategy(
+    { usernameField: 'email'}, // specify the field name here. Telling passport that my username field is called email
+    async function verify(email, password, cb){
+    console.log("IN lOCAL Strategy");
+    
+    //When ever passport.authenticate is called the it already has access to the form data, you do not need body parser , So the email and password are being passed as parameters to this verify function. NOTE THE NAME OF THE INPUTS HAVE TO BE THE SAME NAME AS THE PARAMETERS!!
+    try {
+        console.log(email);
+        console.log("HEYYY");
+        const result = await db.query("SELECT * FROM users WHERE email = $1", [email]);
+        if (result.rows.length > 0){
+            const user = result.rows[0];
+            const storedHashedPassword = user.password;
+            console.log(user.email);
+
+            bcrypt.compare(password,storedHashedPassword, (err, result)=>{
+                if (err){
+                     //Error with password check
+                    cb(err, false, {message: "Error checking password"});
+                }else{
+                    //result is a boolean which is either true or false depending on whether the passwords match or not.
+                    if (result){
+                        console.log("Passwords match");
+                        cb(null, user);
+                    }else{
+                        cb(null,false, {message: "Invalid Password"});
+                    }
+                }
+            })
+        }else{
+            cb(null, false, {message: "User not found"});
+        }
+;    }catch(err){
+        console.log(err);
+        cb(err, false, {message: "Error with databse query"});
+    }
+}));
+
+passport.serializeUser((user, cb) => {
+    cb(null, user);
+  });
+  passport.deserializeUser((user, cb) => {
+    cb(null, user);
+  });
 
 app.listen(port, () => {
-    console.log("Running on Port 3000")
+    console.log("Running on Port 3000");
 })
