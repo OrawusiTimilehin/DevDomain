bb327664d7a4b06b20373cf397f09d8f1d9a9861
OrawusiTimilehin <oluwatimilehin.orawusi@gmail.com>
2024-07-02 15:15:41 +0100

    Created comment functionality

--- index.js
+++ index.js
@@ -42,6 +42,12 @@ app.use((req, res, next) => {
     next();
   });
 
+  
+app.use((req, res, next) => {
+    res.locals.authenticated = req.isAuthenticated();
+    next();
+});
+
 
 // Initialization of the db object
 const db = new pg.Client({
@@ -149,14 +155,17 @@ app.post("/signup", async (req, res)=>{
 
 app.get("/post/:id", async (req, res )=>{
     if (req.isAuthenticated()){
-        console.log("Entered into the route handler")
+        console.log("Entered into the route handler");
     const id = req.params.id;
     console.log(`This is the id ${id}`);
-    const result = await db.query("SELECT * FROM posts WHERE id = $1", [id]);
-    console.log(result.rows);
+    const post = await db.query("SELECT * FROM posts WHERE id = $1", [id]);
+    console.log(post);
+    const comments = await db.query("SELECT * FROM comments WHERE post_id = $1", [id]);
+    console.log(comments);
     res.render("article.ejs", {
-        post: result.rows[0],
-        authenticated: req.isAuthenticated()
+        post: post.rows[0],
+        authenticated: req.isAuthenticated(), 
+        comments: comments.rows
     });
     }else{
         res.redirect("/login")
@@ -192,6 +201,111 @@ app.post("/add-post", async (req, res)=>{
 
 })
 
+app.get("/profile", async(req, res)=>{
+    console.log('Authenticated user:', req.user);  
+    const userId = req.user.id;
+    try{
+        const result = await db.query("SELECT * FROM users WHERE id = $1", [userId]);
+        const user = result.rows[0];
+        res.render("profile.ejs", {
+            username : user.username,
+            occupation: user.occupation,
+            company: user.company,
+            skills: user.skills,
+            linkedin : user.linkedin,
+            github: user.github,
+            twitter: user.twitter,
+            authenticated: req.isAuthenticated()
+        })
+    }catch(err){
+        console.log(err);
+        res.redirect("profile.ejs", {
+            error: "Error rendering profile",
+            authenticated: req.isAuthenticated()
+        });
+    }
+    
+});
+app.get("/update-tech-profile", (req, res)=>{
+    res.render("update-tech-profile.ejs");
+});
+app.get("/update-username", (req, res)=>{
+    const user = req.user;
+    res.render("update-username.ejs", {
+        username: req.user.username
+    });
+});
+app.get("/update-links", (req, res)=>{
+    res.render("update-links.ejs");
+});
+app.post("/edit-username", async (req, res) => {
+    const username = req.body.username;
+    const userId = req.user.id;
+    try{
+        const result = await db.query("SELECT * FROM users WHERE username = $1", [username]);
+        if (result.rows == 0){
+            try{
+                const result = await db.query("UPDATE users SET username = $1 WHERE id = $2", [username, userId]);
+                res.redirect("/profile");
+            }catch(err){
+                console.log(err);
+                res.render("update-username.ejs", {
+                    error: "Internal Server Error"
+                });
+            }
+        }else{
+            res.render("update-username.ejs", {
+                error: "Username already exists"
+            });
+        }
+    }catch(err){
+        console.log(err);
+        res.render("update-username.ejs", {
+            error: "Internal Server Error"
+        })
+    }
+   
+})
+
+app.post("edit-links", async (req, res) => {
+    const userId = req.user.id;
+    const github = req.body.github;
+    const linkedin = req.body.linkedin;
+    const twitter = req.body.twitter;
+    try{
+        const result = await db.query("UPDATE users SET github_url = $1, linkedin_url = $2, twitter_url = $3 WHERE id = $4", [github, linkedin, twitter, userId]);
+    }catch(err){
+        console.log(err)
+        res.redirect("/update-links");
+    }
+});
+
+app.post("edit-tech-profile", async (req, res) => {
+    const userId = req.user.id;
+    const occupation = req.body.occupation;
+    const skills = req.body.skills;
+    const company = req.body.company;
+    try{
+        const result = await db.query("UPDATE users SET occupation = $1, skills = $2, company = $3 WHERE id = $4", [occupation, skills, company, userId]);
+    }catch(err){
+        console.log(err)
+        res.redirect("/update-tech-profile");
+    }
+});
+
+app.post("/comment", async(req, res) => {
+    const comment = req.body.comment;
+    const postId = req.body.post_id;
+    const userId = req.user.id;
+    try {
+        const result = await db.query("INSERT INTO comments (user_id, post_id, content) VALUES ($1, $2, $3)", [userId, postId, comment]);
+        res.redirect("/post/" + postId);
+    }catch(err){
+        console.log(err);
+        res.redirect("/post/" + postId);
+    }
+   
+});
 //passport local strategy 
 
 //NOTE : for error messages to show in the 'info' part of the cb it must be an object with 'message: error'. It must be called message.
